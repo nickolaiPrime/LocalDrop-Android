fe650b2a5cdb44b70727da031872186e94a6ae07
Николай <bezintellekta@gmail.com>
2026-07-02 20:23:12 +0500

    feat: implement chatcreen with message list and input panel

--- app/src/main/java/com/example/localdrop/MainActivity.kt
+++ app/src/main/java/com/example/localdrop/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -30,6 +32,7 @@ import com.example.localdrop.presentation.viewmodel.DiscoveryUiState
 import com.example.localdrop.presentation.viewmodel.MainViewModel
 import org.koin.androidx.compose.koinViewModel
 import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.Alignment
 import com.example.localdrop.domain.model.NetworkDevice
 
 class MainActivity : ComponentActivity() {
@@ -51,12 +54,24 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                     else{
-                        MainDropScreen(
-                            uiState = uiState,
-                            onDeviceSelected = { device->
-                                Log.d("MainActivity", "Выбрано устройство: ${device.ipAddress}")
-                            }
-                        )
+                        if(uiState.selectedDevice == null){
+                            MainDropScreen(
+                                uiState = uiState,
+                                onDeviceSelected = { device->
+                                    Log.d("MainActivity", "Выбрано устройство: ${device.ipAddress}")
+                                    viewModel.selectDevice(device)
+                                }
+                            )
+                        }
+                        else{
+                            ChatScreen(
+                                uiState = uiState,
+                                onBackClick = { viewModel.goBackToDeviceList() },
+                                onSendClick = { text ->
+                                    Log.d("MainActivity", "Хотим отправить: $text")
+                                }
+                            ){}
+                        }
                     }
                 }
             }
@@ -140,4 +155,75 @@ fun MainDropScreen(
             }
         }
     }
+}
+
+@Composable
+fun ChatScreen(
+    uiState : DiscoveryUiState,
+    onBackClick: () -> Unit,
+    onSendClick: (String) -> Unit
+){
+    var inputText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+
+        Button(
+            onClick = onBackClick
+        ){
+            Text(text = "Назад")
+        }
+
+        Text(
+            text = "Чат с устройством: ${uiState.selectedDevice?.ipAddress}",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ){
+            items(uiState.messages){ message ->
+                Text(
+                    text = message.text,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            OutlinedTextField(
+                value = inputText,
+                onValueChange = {inputText = it},
+                modifier = Modifier.weight(1f),
+                placeholder = { Text(text = "Введите сообщение...") }
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .width(8.dp)
+            )
+
+            Button(
+                onClick = {
+                    if(inputText.isNotBlank()){
+                        onSendClick(inputText)
+                        inputText = ""
+                    }
+                }
+            ){
+                Text(text = "Отправить")
+            }
+        }
+    }
+
 }
