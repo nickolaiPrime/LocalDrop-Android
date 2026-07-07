3ba8f195bb6ac2078563c2a1bde438ed3968dd9a
Николай <bezintellekta@gmail.com>
2026-07-07 21:40:38 +0500

    fix: resolve self-discovery duplicate and fix network device filtering by ip

--- app/src/main/java/com/example/localdrop/data/repository/NsdDiscoveryImpl.kt
+++ app/src/main/java/com/example/localdrop/data/repository/NsdDiscoveryImpl.kt
@@ -9,8 +9,17 @@ import android.net.nsd.NsdServiceInfo
 import android.util.Log
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
+import java.net.NetworkInterface
+import java.util.Collections
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 
 class NsdDiscoveryImpl(private val context : Context) : NetworkDiscoveryRepository{
+
+    val wifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as android.net.wifi.WifiManager
+    val multicastLock = wifiManager.createMulticastLock("LocalDropLock").apply {
+        setReferenceCounted(false)
+    }
     private val registrationListener = object : NsdManager.RegistrationListener{
         override fun onServiceRegistered(nsdServiceInfo: NsdServiceInfo?) {
             Log.d("NSD_TAG", "Сервис успешно зарегистрирован")
@@ -34,7 +43,7 @@ class NsdDiscoveryImpl(private val context : Context) : NetworkDiscoveryReposito
     override suspend fun startBroadcasting(id: String, port : Int) {
         val serviceInfo = NsdServiceInfo().apply {
             serviceName = id
-            serviceType = "_localDrop._tcp."
+            serviceType = "_localdrop._tcp."
             this.port = port
         }
         nsdManager.registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener)
@@ -46,7 +55,10 @@ class NsdDiscoveryImpl(private val context : Context) : NetworkDiscoveryReposito
 
     override fun startDiscovery(myDeviceName : String): Flow<List<NetworkDevice>> {
         return callbackFlow {
-            val discoveredDevices = mutableSetOf<NetworkDevice>()
+
+            val discoveredDevices = ConcurrentHashMap<String, NetworkDevice>()
+            val myCurrentIp = getLocalIpAddress()
+
             val discoveryListener = object : NsdManager.DiscoveryListener{
                 override fun onDiscoveryStarted(message: String?) {
                     Log.d("NSD_TAG", message?: "")
@@ -55,9 +67,9 @@ class NsdDiscoveryImpl(private val context : Context) : NetworkDiscoveryReposito
                 override fun onDiscoveryStopped(message: String?) {
                     Log.d("NSD_TAG", message?: "")
                 }
+
                 @Suppress("DEPRECATION")
                 override fun onServiceFound(serviceInfo: NsdServiceInfo) {
-                    if(myDeviceName == serviceInfo.serviceName) return
                     nsdManager.resolveService(serviceInfo, object : NsdManager.ResolveListener {
                         override fun onResolveFailed(serviceInfo: NsdServiceInfo?, errorCode: Int) {
                             Log.d("NSD_TAG", "Не удалось разрешить адрес: $errorCode")
@@ -66,21 +78,27 @@ class NsdDiscoveryImpl(private val context : Context) : NetworkDiscoveryReposito
                         override fun onServiceResolved(resolvedInfo: NsdServiceInfo) {
                             val ip = resolvedInfo.host?.hostAddress ?: ""
 
+                            if(ip.isNotEmpty() && ip == myCurrentIp){
+                                Log.d("NSD_TAG", "Фильтр собственного IP: $ip")
+                                return
+                            }
+
+
                             val device = NetworkDevice(
                                 name = resolvedInfo.serviceName,
                                 ipAddress = ip,
                                 port = resolvedInfo.port
                             )
 
-                            discoveredDevices.add(device)
-                            trySend(discoveredDevices.toList())
+                            discoveredDevices[resolvedInfo.serviceName] = device
+                            trySend(discoveredDevices.values.toList())
                         }
                     })
                 }
 
                 override fun onServiceLost(serviceInfo: NsdServiceInfo) {
-                    discoveredDevices.removeAll { it.name == serviceInfo.serviceName}
-                    trySend(discoveredDevices.toList())
+                    discoveredDevices.remove(serviceInfo.serviceName)
+                    trySend(discoveredDevices.values.toList())
                 }
 
                 override fun onStartDiscoveryFailed(message: String?, errorCode: Int) {
@@ -92,12 +110,35 @@ class NsdDiscoveryImpl(private val context : Context) : NetworkDiscoveryReposito
                 }
 
             }
-            nsdManager.discoverServices("_localDrop._tcp.", NsdManager.PROTOCOL_DNS_SD, discoveryListener)
-            awaitClose { nsdManager.stopServiceDiscovery(discoveryListener)}
+            multicastLock.acquire()
+            nsdManager.discoverServices("_localdrop._tcp.", NsdManager.PROTOCOL_DNS_SD, discoveryListener)
+            awaitClose {
+                nsdManager.stopServiceDiscovery(discoveryListener)
+                if (multicastLock.isHeld) multicastLock.release()
+            }
         }
     }
 
     override suspend fun stopDiscovery() {
         TODO("Not yet implemented")
+    }
+
+    private fun getLocalIpAddress(): String {
+        try {
+            val interfaces = Collections.list(NetworkInterface.getNetworkInterfaces())
+            for (networkInterface in interfaces) {
+                val addresses = Collections.list(networkInterface.inetAddresses)
+                for (address in addresses) {
+                    if (!address.isLoopbackAddress) {
+                        val sAddr = address.hostAddress
+                        val isIPv4 = sAddr.indexOf(':') < 0 // Если нет двоеточий, это IPv4
+                        if (isIPv4) return sAddr
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("NSD_TAG", "Не удалось получить собственный IP: ${e.message}")
+        }
+        return ""
     }
 }
